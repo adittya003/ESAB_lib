307e61ab27554eaab556f1be80f0ccd66b2e5b93
Adittya Narayan <adittyanarayan76@gmail.com>
2023-11-02 11:19:29 +0530

    qr_return and qr_borrow done

--- app/routes.py
+++ app/routes.py
@@ -2,13 +2,14 @@ from flask import render_template,redirect,url_for,request
 from . import app
 from .form import * 
 from .models import *
+from datetime import date, timedelta
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/user_qrscan', methods=["POST","GET"])
-def user_qrscan():
+@app.route('/user_qrscan_borrow', methods=["POST","GET"])
+def user_qrscan_borrow():
     form=UserQrScan()
     if form.validate_on_submit():
         user = User.query.filter_by(
@@ -17,12 +18,12 @@ def user_qrscan():
         if user is None:
             return "Invalid QR"
         
-        return redirect(url_for('book_qrscan',userid=user.id))
-    return render_template('user_qr.html',form=form)
+        return redirect(url_for('book_qrscan_borrow',userid=user.id))
+    return render_template('user_qr_borrow.html',form=form)
 
 
-@app.route('/book_qrscan', methods=["POST","GET"])
-def book_qrscan():
+@app.route('/book_qrscan_borrow', methods=["POST","GET"])
+def book_qrscan_borrow():
     form=BookQrScan()
 
     user_id = request.args.get('userid')
@@ -54,6 +55,63 @@ def book_qrscan():
         db.session.add(trans)
         db.session.commit()
 
-        return "Issued"
+        return "Issued",trans.id
     
-    return render_template('book_qr.html',form=form,userid=user_id)
+    return render_template('book_qr_borrow.html',form=form,userid=user_id)
+
+
+
+
+@app.route('/user_qrscan_return', methods=["POST","GET"])
+def user_qrscan_return():
+    form=UserQrScan()
+    if form.validate_on_submit():
+        user = User.query.filter_by(
+            qr=form.data.get('qr')
+        ).first()
+        if user is None:
+            return "Invalid QR"
+        
+        return redirect(url_for('book_qrscan_return',userid=user.id))
+    return render_template('user_qr_return.html',form=form)
+
+
+@app.route('/book_qrscan_return', methods=["POST","GET"])
+def book_qrscan_return():
+    form=BookQrScan()
+
+    user_id = request.args.get('userid')
+    if user_id is None:
+        return "No User ID Present"
+    
+    if form.validate_on_submit():
+        book_qr = form.data.get("qr")
+
+        user = User.query.filter_by(
+            id = user_id
+        ).first()
+
+        book = Books.query.filter_by(
+            qr = book_qr
+        ).first()
+
+        if user is None or book is None:
+            return "Error"
+        
+        if not book.issue:
+            return "Book Already Returned"
+        
+        trans=Transaction.query.filter_by(user_id=user.id,book_id=book.id,return_date=None).first()
+        if trans:
+            trans.return_date = date.today()
+            trans.returned=True
+            book.issue=False
+            db.session.add(trans)
+            db.session.commit()
+            return "Returned"
+        
+        else:
+            return "No Transaction Found"
+    
+    return render_template('book_qr_return.html',form=form,userid=user_id)
+
